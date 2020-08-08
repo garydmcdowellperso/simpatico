1bf05f507e951e615a091b4356df62a32c2553ea
garydmcdowell <gary.mcdowell@skwirrel.fr>
2020-08-08 14:06:29 +0200

    Logging

--- api/src/services/iam/lib/use_cases/UpdateUserInfo.js
+++ api/src/services/iam/lib/use_cases/UpdateUserInfo.js
@@ -1,5 +1,4 @@
 async function UpdateUserInfo(id, firstName, lastName, email, bio, avatar, { userRepository }) {
-  console.log('bio', bio)
   if (!id) {
     throw new Error("No id");
   }
@@ -8,12 +7,6 @@ async function UpdateUserInfo(id, firstName, lastName, email, bio, avatar, { use
   }
   if (!lastName) {
     throw new Error("No lastName");
-  }
-  if (!bio) {
-    throw new Error("No bio");
-  }
-  if (!avatar) {
-    throw new Error("No avatar");
   }
 
   const user = await userRepository.get(id);
