e8d99d016465249d4bd5c194f4559533ba1146e3
garydmcdowell <gary.mcdowell@skwirrel.fr>
2020-08-08 15:55:05 +0200

    Updates

--- api/src/api/iam/index.js
+++ api/src/api/iam/index.js
@@ -660,9 +660,9 @@ const routes = async fastify => {
             });
 
             if (response.token.role.includes('administrator')) {
-                reply.redirect(`/admin/?token=${response.token.token}`);
+                reply.redirect(`/admin/`);
             } else {
-                reply.redirect(`/?token=${response.token.token}`);
+                reply.redirect(`/`);
             }
         }
     );
