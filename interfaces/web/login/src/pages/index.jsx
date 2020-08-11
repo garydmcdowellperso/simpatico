49f6f9d6912c3c0d40a03e5653796eb61e8f09bf
garydmcdowell <gary.mcdowell@skwirrel.fr>
2020-08-11 10:03:37 +0200

    favicon

--- interfaces/web/login/src/pages/index.jsx
+++ interfaces/web/login/src/pages/index.jsx
@@ -8,7 +8,7 @@ const Home = (debate) => {
     <div className='content'>
       <Head>
         <title>Login</title>
-        <link rel="icon" href="/static/favicon.ico" />
+        <link rel="icon" href="/login/static/favicon.ico" />
         <link
           rel="stylesheet"
           href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
