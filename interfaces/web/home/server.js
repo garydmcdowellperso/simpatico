7d964cd7a8fe17594398a6d679195c5b39f2e046
garydmcdowell <gary.mcdowell@skwirrel.fr>
2020-08-12 15:26:15 +0200

    Remove local storage usage

--- interfaces/web/home/server.js
+++ interfaces/web/home/server.js
@@ -24,7 +24,7 @@ const start = () => {
 
       server.use(nextI18NextMiddleware(nextI18next));
 
-      server.use("/home/static", express.static("static"));
+      server.use("/static", express.static("static"));
 
       // healthcheck route for k8s
       server.get("/v1/ping", (req, res) => {
