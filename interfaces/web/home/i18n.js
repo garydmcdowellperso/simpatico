b2612c43cf56a7ca7a6159c350af577d5c91ae11
garydmcdowell <gary.mcdowell@skwirrel.fr>
2020-08-12 16:19:37 +0200

    Remove local storage usage

--- interfaces/web/home/i18n.js
+++ interfaces/web/home/i18n.js
@@ -1,10 +1,7 @@
 import NextI18Next from 'next-i18next';
-import translationEN from './static/locales/en/common.json';
-import translationFR from './static/locales/fr/common.json';
 
 import config from './config';
 
-console.log('translationEN', translationEN)
 const {
   i18n: { languages, defaultLanguage },
 } = config;
@@ -17,21 +14,12 @@ const detectorOptions = {
   caches: []
 };
 
-const resources = {
-  en: {
-    translation: translationEN
-  },
-  fr: {
-    translation: translationFR
-  }
-};
-
 export default new NextI18Next({
-  resources,
   otherLanguages: languages,
   fallbackLng: fallbackLanguage,
   defaultNS: 'common',
-  detection: detectorOptions
+  detection: detectorOptions,
+    
 });
 
 // known bug showing 'react-i18next:: i18n.languages were undefined or empty undefined'
